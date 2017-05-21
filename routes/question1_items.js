17057a0395b680dd0e8104fd93f0efa7c316c4da
Xu Li <xuli0124@gmail.com>
2017-05-20 22:14:58 -0700

    fix bugs

--- routes/question1_items.js
+++ routes/question1_items.js
@@ -3,7 +3,14 @@ var data = require('../question1.json');
 exports.view = function (req, res) {
 	var id = req.params.id;
 	console.log("The item id is: " + id);
-
+	for (i = 0; i < data.places.length; i++){
+		if (data.places[i].id == id){
+					var content = data.places[i].content;
+					var image = data.places[i].image;
+					var link = data.places[i].link;
+					//console.log(content);
+			}
+	}
 	for (i = 0; i < data.food.length; i++){
       if (data.food[i].id == id){
             var content = data.food[i].content;
@@ -13,20 +20,13 @@ exports.view = function (req, res) {
         }
     }
 
-    for (i = 0; i < data.places.length; i++){
-      if (data.places[i].id == id){
-            var content = data.places[i].content;
-						var image = data.places[i].image;
-            var link = data.places[i].link;
-            //console.log(content);
-        }
-    }
+
 
 		for (i = 0; i < data.accommodation.length; i++){
-      if (data.places[i].id == id){
+      if (data.accommodation[i].id == id){
             var content = data.accommodation[i].content;
 						var image = data.accommodation[i].image;
-            var link = data.plaaccommodationces[i].link;
+            var link = data.accommodation[i].link;
             //console.log(content);
         }
     }
